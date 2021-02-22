847b80c73b8ef99f095b35374da8d18989bee00a
6y6lek <49441978+6y6lek@users.noreply.github.com>
2021-02-22 12:58:12 +0200

    Update search.py

--- dfs, bfs/search.py
+++ dfs, bfs/search.py
@@ -147,11 +147,56 @@ def nullHeuristic(state, problem=None):
     return 0
 
 def aStarSearch(problem, heuristic=nullHeuristic):
-    """
-    Q1.3
-    Search the node that has the lowest combined cost and heuristic first."""
-    """Call heuristic(s,problem) to get h(s) value."""
-    "*** YOUR CODE HERE ***"
+    start = problem.getStartState()
+    exploredState = []
+    states = util.PriorityQueue()
+    states.push((start, []), nullHeuristic(start, problem))
+    nCost = 0
+    while not states.isEmpty():
+        state, actions = states.pop()
+        if problem.isGoalState(state):
+            return actions
+        if state not in exploredState:
+            successors = problem.getSuccessors(state)
+            for succ in successors:
+                coordinates = succ[0]
+                if coordinates not in exploredState:
+                    directions = succ[1]
+                    nActions = actions + [directions]
+                    nCost = problem.getCostOfActions(nActions) + heuristic(coordinates, problem)
+                    states.push((coordinates, actions + [directions]), nCost)
+        exploredState.append(state)
+    return actions
+    util.raiseNotDefined()
+
+from collections import defaultdict
+
+def greedySearch(problem, heuristic=nullHeuristic):
+    visited = set()
+    came_from = {}
+    start = problem.getStartState()
+
+    h_score = defaultdict(lambda: float('inf'))
+    h_score[start] = heuristic(start, problem)
+
+    queue = util.PriorityQueue()
+    queue.push(start, h_score[start])
+    while not queue.isEmpty():
+        s = queue.pop()
+        if problem.isGoalState(s):
+            path = []
+            while s != start:
+                p, d = came_from[s]
+                path.append(d)
+                s = p
+            return path[::-1]
+        visited.add(s)
+        for u, d, c in problem.getSuccessors(s):
+            if u in visited:
+                continue
+            h_score[u] = heuristic(u, problem)
+            queue.push(u, h_score[u])
+            came_from[u] = (s, d)
 
 def priorityQueueDepthFirstSearch(problem):
     """
@@ -191,3 +236,4 @@ dfs = depthFirstSearch
 astar = aStarSearch
 bfs2 = priorityQueueBreadthFirstSearch
 dfs2 = priorityQueueDepthFirstSearch
+greedy = greedySearch
